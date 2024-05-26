ab45a52fd78710bb54910d4ad58f247bbcbef39c
Max46656 <117962384+Max46656@users.noreply.github.com>
2024-05-26 22:17:38 +0800

    Update 脆的千喜問題.js
    更新startObserving()中observe()的參數，取消不必要的childList，
    並新增characterData，以對應Ajax直接更改innerText而不更改元素的狀況。
    更新handleNewPosts()為handleAllPosts()以符合實際作用，
    在執行修改文字前需呼叫stopObserving()以避開觸發characterData更動。
    更新this.interactStatClassName，以同時符合「已互動」與「未互動」兩種classname。
    新增window.onload呼叫handleNewPosts()以清除載入時就存在的錯誤。

--- 脆的千喜問題.js
+++ 脆的千喜問題.js
@@ -4,7 +4,7 @@
 // @name:en     ThreadsS1KProblem
 // @description  刪除Threads的互動統計數據中超過1K時會出現的".0"，其應是整數而非浮點數。
 // @namespace    https://github.com/Max46656
-// @version      1.0
+// @version      1.2
 // @author       Max
 // @description:ja Threads のインタラクション統計データで、1K を超える場合に表示される「.0」を削除し、整數にする必要があります。
 // @description:en Delete ".0" in Threads interact stat when it's over 1K, which should be an integer instead of a float.
@@ -12,46 +12,52 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=threads.net
 // @grant        none
 // @license MPL2.0
+// @downloadURL https://update.greasyfork.org/scripts/496055/%E8%84%86%E7%9A%84%E5%8D%83%E5%96%9C%E5%95%8F%E9%A1%8C.user.js
+// @updateURL https://update.greasyfork.org/scripts/496055/%E8%84%86%E7%9A%84%E5%8D%83%E5%96%9C%E5%95%8F%E9%A1%8C.meta.js
 // ==/UserScript==
 
 
 class InteractStatChanger {
     constructor() {
-        this.interactStatObserver = new MutationObserver(this.handleNewPosts.bind(this));
-        //空格以搜尋符合條件的父元素下符合條件的子元素
-        this.interactStatClassName='.x6ikm8r.x10wlt62 .x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xmd891q.xo1l8bm.xc82ewx.x1yc453h';
+        this.interactStatObserver = new MutationObserver(this.handleAllPosts.bind(this));
+        // 空格以搜尋符合條件的父元素下符合條件的子元素，將class中的空格以"."取代
+        this.interactStatClassName='div.x6ikm8r.x10wlt62 span.x1lliihq.x1plvlek.xryxfnj.x1n2onr6.x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.x1i0vuye.xmd891q.xo1l8bm';
     }
 
     startObserving() {
-        this.interactStatObserver.observe(document.body, { subtree: true, childList: true, attributes: true });
+        this.interactStatObserver.observe(document.body, { subtree: true, childList: false, characterData: true });
     }
 
     stopObserving() {
         this.interactStatObserver.disconnect();
     }
 
-    handleNewPosts(mutationsList) {
+    handleAllPosts() {
+        this.stopObserving();
+
         const interactStatElements = document.querySelectorAll(this.interactStatClassName);
+        console.log('interactStat有', interactStatElements.length, '個 at ', new Date().toLocaleString());
         interactStatElements.forEach(element => {
             Array.from(element.childNodes).forEach(node => {
-                node.nodeValue=this.toInteger(node);
+                node.nodeValue = this.toInteger(node);
             });
         });
+
+        this.startObserving();
     }
 
-    toInteger(node){
+    toInteger(node) {
         if (node.nodeType !== Node.TEXT_NODE || !node.nodeValue.match(/\.0/g)) {
-            return node.nodeValue
-        }else {
-            console.log('L1KProblemFound');
-            return node.nodeValue.replace(/\.0/g, '');
+            return node.nodeValue;
+        } else {
+            console.log('L1KProblemFound ', node.nodeValue);
+            return node.nodeValue.replace(/\.0/, '');
         }
     }
-
 }
-
 
 const johnTheMathTeacher = new InteractStatChanger();
 johnTheMathTeacher.startObserving();
-
-
+window.onload = (event) => {
+  johnTheMathTeacher.handleAllPosts();
+};
